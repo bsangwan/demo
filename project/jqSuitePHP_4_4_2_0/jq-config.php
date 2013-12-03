6e29af4973ac5961901d93d2566c80f295bf25b1
jnuc093 <jnuc093@gmail.com>
2013-12-03 20:37:08 +0800

    jqgrid table

--- project/jqSuitePHP_4_4_2_0/jq-config.php
+++ project/jqSuitePHP_4_4_2_0/jq-config.php
@@ -4,7 +4,7 @@
 //define('DB_HOST', 'localhost');    // 99% chance you won't need to change this value
 define('DB_DSN','mysql:host=localhost;dbname=northwind');
 define('DB_USER', 'root');     // Your MySQL username
-define('DB_PASSWORD', ''); // ...and password
+define('DB_PASSWORD', 'root'); // ...and password
 
 define('ABSPATH', dirname(__FILE__).'/');
 //require_once(ABSPATH.'tabs.php');
