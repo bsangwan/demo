0896f5bb02a5edd77eaa04ae674376db3c62272c
jnuc093 <jnuc093@gmail.com>
2015-03-03 14:16:57 +0800

    oracle测试文件

---
+++ py/hyry/oracle.py
@@ -0,0 +1,10 @@
+import cx_Oracle
+
+conn = cx_Oracle.connect('zhgl/zhgl@192.168.1.107:1521/orcl')
+cursor = conn.cursor ()
+cursor.execute ("select * from dual")
+row = cursor.fetchone ()
+print row[0]
+
+cursor.close ()
+conn.close ()
