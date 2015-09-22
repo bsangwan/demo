c7c4eeefec4b90f0dfd8df6298444b8276f0e801
jnuc093 <jnuc093@gmail.com>
2015-09-22 22:47:33 +0800

    todo-aholic

---
+++ 3_angular/todo-aholic/config/database.js
@@ -0,0 +1,6 @@
+module.exports = {
+
+	// the database url to connect
+	// url : 'mongodb://root:root@mongo.onmodulus.net:27017/uwO3mypu'
+	url : 'mongodb://root:root@jello.modulusmongo.net:27017/Xorap2ev'
+}
--- 3_angular/todo-aholic/server.js
+++ 3_angular/todo-aholic/server.js
@@ -1,85 +1,27 @@
 // set up ==========================
-var express = require('express');
-var app 	= express();
+var express  = require('express');
+var app 	 = express();
 var mongoose = require('mongoose');
-var morgan = require('morgan');
+var port     = process.env.PORT || 9080;
+var database = require('./config/database');
+var morgan   = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
-var Todo = require('./app/models/todo')
-
+                
 // configuration ===================
-
-mongoose.connect('mongodb://root:root@jello.modulusmongo.net:27017/Xorap2ev');     // connect to mongoDB database on modulus.io
+mongoose.connect(database.url);
 
 app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({'extended':'true'}))
 app.use(bodyParser.json())
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
-app.use(methodOverride());
+app.use(methodOverride('X-HTTP-Method-Override'));
+
+
+// routes ==========================================
+require('./app/routes.js')(app);
 
 // listen (start app with node server.js)===
-app.listen(8080)
-console.log("App listening on port 8080");
-
-// routes ======================================================================
-
-    // api ---------------------------------------------------------------------
-    // get all todos
-    app.get('/api/todos', function(req, res) {
-
-        // use mongoose to get all todos in the database
-        Todo.find(function(err, todos) {
-        	console.log('todos1111=');
-        	console.log('todos='+todos);
-            // if there is an error retrieving, send the error. nothing after res.send(err) will execute
-            if (err)
-                res.send(err)
-
-            res.json(todos); // return all todos in JSON format
-        });
-    });
-
-    // create todo and send back all todos after creation
-    app.post('/api/todos', function(req, res) {
-
-        // create a todo, information comes from AJAX request from Angular
-        Todo.create({
-            text : req.body.text,
-            done : false
-        }, function(err, todo) {
-            if (err)
-                res.send(err);
-
-            // get and return all the todos after you create another
-            Todo.find(function(err, todos) {
-                if (err)
-                    res.send(err)
-                res.json(todos);
-            });
-        });
-
-    });
-
-    // delete a todo
-    app.delete('/api/todos/:todo_id', function(req, res) {
-        Todo.remove({
-            _id : req.params.todo_id
-        }, function(err, todo) {
-            if (err)
-                res.send(err);
-
-            // get and return all the todos after you create another
-            Todo.find(function(err, todos) {
-                if (err)
-                    res.send(err)
-                res.json(todos);
-            });
-        });
-    });
-
-
-// application ==================================
-app.get('*',function(req,res){
-	res.sendfile('./public/index.html');
-});
+app.listen(port)
+console.log("App listening on port "+ port);
