fe685e5b27edbe6feba403ba7121b9928fa709d4
jnuc093 <jnuc093@gmail.com>
2015-10-19 10:04:47 +0800

    it should have a title

--- project/2_protractortest/testscript/spec.js
+++ project/2_protractortest/testscript/spec.js
@@ -1,34 +1,7 @@
 describe('Protractor Demo App', function() {
-  var firstNumber = element(by.model('first'));
-  var secondNumber = element(by.model('second'));
-  var goButton = element(by.id('gobutton'));
-  var latestResult = element(by.binding('latest'));
-  var history = element.all(by.repeater('result in memory'));
 
-  function add(a,b){
-    firstNumber.sendKeys(a);
-    secondNumber.sendKeys(b);
-    goButton.click();
-  }
-
-  beforeEach(function() {
-    browser.get('http://juliemr.github.io/protractor-demo/');
-  });
-
-  it('should have a history', function() {
-    add(1,2);
-    add(3,4);
-
-  expect(history.count()).toEqual(2);
-  expect(history.first().getText()).toContain('3 + 4');
-
-
-  add(5,6);
-  add(7,8);
-
-  expect(history.count()).toEqual(4);
-
-  expect(history.last().getText()).toContain('1 + 2');
-  expect(history.first().getText()).toContain('7 + 8');
-  });
+	it('should have a title',function(){
+		    browser.get('http://10.1.111.180:3000/#/');
+		      expect(browser.getTitle()).toEqual('IRIS horizon');
+	});
 });
