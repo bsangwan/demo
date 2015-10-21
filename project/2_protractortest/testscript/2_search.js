96c4bc8063cc32f409b4ffde26f51f60d1829fcf
jnuc093 <jnuc093@gmail.com>
2015-10-21 11:43:15 +0800

    multi select

--- project/2_protractortest/testscript/2_search.js
+++ project/2_protractortest/testscript/2_search.js
@@ -20,21 +20,34 @@ describe('Add New Batch', function() {
     }
   };
 
-	
+  
+    
 
 	//http://www.ngroutes.com/questions/18ad2a8/protractor-looping-through-table-to-select-option-from-listbox.html
 	it('2_search',function(){
 
         element(by.id('JobTypeSearch')).click();
         // element(by.cssContainingText('option','HB')).click();
+        // BatchNumbers
         element.all(by.css('select[name="BatchNumbers"] option')).then(function(arr){
             arr.forEach(function(v){
                 v.click().then(function(){
-                    console.log("Call clicking");
+                    console.log("Call clicking option");
                 })
-                browser.sleep(10000);
+                
             })
         })
-	}); //it add-new-batch end
+
+        // Template
+        element.all(by.css('select[name="Template"] option')).then(function(arr){
+            arr.forEach(function(v){
+                v.click().then(function(){
+                    console.log("Call clicking template option");
+                })
+            })
+        })
+
+        // browser.sleep(10000);
+	}); //search end
 
 });
