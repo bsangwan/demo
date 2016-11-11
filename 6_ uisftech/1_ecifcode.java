3608d018a8209ece87fb7c987f451fa4fe95b0c6
jnuc093 <jnuc093@gmail.com>
2016-11-11 19:44:51 +0800

    个人客户信息全量修改

--- 6_ uisftech/1_ecifcode.java
+++ 6_ uisftech/1_ecifcode.java
@@ -10,15 +10,6 @@ BusinessPersonManagerUpdateListHelper.updatePersonFamilyMemberInfo()
 证件信息校验
 BusinessCreatePersonHelper                
 
-修改联系信息
-
-查询家庭成员信息:				
-BusinessQueryPersonHelper.
-queryPersonMemberInfo()				
-
-BusinessPersonMemberInfoManageServices.
-queryPersonMemberInfo()
-
 对公:
 修改:
 BusinessPartnerGroupUpdateServices.
@@ -37,6 +28,8 @@ GroovyServiceTest
 child_case
 
 getPersonSequence()
+
+updatePersonalInfo()
 
 
 getPartyAllContactInfo()
@@ -63,29 +56,71 @@ Helper类:
 修改总集合=================         BusinessUpdatePersonHelper         doOperateWhatUpdate()
 个人客户信息修改基本信息修改服务         BusinessUpdatePersonHelper         updatePersonDetail()
 
-修改联系信息================
-    runSync("updateContactInfo",contactInfoMap, 0, false)
-创建或修改联系信息               BusinessPersonManagerUpdateListHelper        updateContactInfo()
----------------               单个联系信息修改-----------------------         updateSingleContactInfo()      
+/**
+ * 修改证件信息
+ */
+dispatcher.runSync("updateContactCertificateInfo", certificateMap, 0, false);
 
-组装地址信息================
-    创建或修改地址信息                BusinessPersonManagerUpdateListHelper        updatePostalAddressInfo()
-    单个联系信息(地址)修改------------------        updateSinglePostalAddressInfo()
+/**
+ * 修改工作单位信息
+ */
 
-组装工作单位信息=============
     组装单个工作单位信息               BusinessUpdatePersonHelper                  updateWorkUnitInfo()
     改用-------------               BusinessPersonManagerUpdateListHelper       updateWorkUnitInfo()
 
-组装控制企业信息=============
-    runSync("updateControlCompanyInfo"
+/**
+ * 修改联系信息 
+ */
+dispatcher.runSync("updateContactInfo",contactInfoMap, 0, false)
+                              BusinessPersonManagerUpdateListHelper        updateContactInfo()
+---------------               单个联系信息修改-----------------------         updateSingleContactInfo()      
+
+// TODO 
+
+/**
+ * 修改家庭成员信息
+ */
+dispatcher.runSync("updateFamilyMemberInfo", memberInfoMap, 0, false);
+                        
+                            BusinessPersonManagerUpdateList            updateFamilyMemberInfo() 
+// TODO  containsKey    Iterator重新生成    OK               
+
+/**
+ * 控制企业信息
+ */
+dispatcher.runSync("updateControlCompanyInfo", memberInfoMap, 0, false);
 
                                 BusinessPersonManagerUpdateList
-修改家庭成员信息==============
-runSync("updateFamilyMemberInfo",familyMemberInfo
+
+/**
+ * 修改关系人信息(社会关系)
+ */
+
+BusinessPersonUpdateRelativeServices
 
 
-----------------               BusinessPersonManagerUpdateList            updateFamilyMemberInfo()  
+/**
+ * 修改地址信息
+ */
+    postalAddressInfoReturnMap = dispatcher.runSync("updateContactPostalAddress", relativeInfoMap, 0, false);
+    此updateContactPostalAddress服务在common.xml里面也有定义
 
+    创建或修改地址信息                BusinessPersonManagerUpdateListHelper        updatePostalAddressInfo()
+    单个联系信息(地址)修改-----------------                                       updateSinglePostalAddressInfo()
+//  TODO 20161109   联系地址是否存在ID判断 循环跑空处理，且需要改成判断containKey
+//  
+
+/**
+ * 修改客户等级,如果升级强度不为空
+ */
+
+
+/**
+ * 个人客户详细信息查询
+ */
+/*************************************************************/
+
+BusinessPersonDetailManageServices.businessQueryPerson()
 
 客户详细基本信息查询=========
 changeDetail(delegator,detaiGenericValue, globalMap);
@@ -94,6 +129,20 @@ BusinessPersonManageServices
 
 查询家庭成员信息
 dispatcher.runSync("queryPersonMemberInfo"
+
+                            BusinessPersonMemberInfoManageServices
+
+queryPersonMemberInfo() 
+
+/**
+ * 联系信息查询服务
+ */
+dispatcher.runSync("queryPersonContactInfo", context, 0, false);
+
+/**
+ * 联系信息(地址)
+ */
+dispatcher.runSync("queryPersonContactAddressInfo", context, 0, false);
 
 String contactAddressType = BusinessQueryPersonHelper.getParamNameToNewValue(delegator, "CONTACT_MECH_TYPE_ID", (String) potalAddress.get("contactAddressType"));
 
@@ -112,4 +161,34 @@ CommonConstants.CONTACT_MECH_TYPE_ID
     		paramMap.put("infoString", contactCertificateNo);
     	}
                 
-(partyIdFrom = '1000000371' AND partyRelationshipTypeId = 'SPOUSE')
+
+Boolean isSingleModify = false;                                         //是否是单个修改           
+            while (memberInfoIterator.hasNext()) {
+                Map<String, Object> contactMap = memberInfoIterator.next();
+                isSingleModify = contactMap.containsKey("partyRelationShipId");     //此处S为大写
+            }
+            if(!isSingleModify){
+                 memberInfoIterator = memberInfoList.listIterator();                //Iterator重新生成  
+
+//联系信息判断
+//判断是否是单个修改
+            Boolean isSingleModify = false;                                                    
+                while (contactInfoIterator.hasNext()) {
+                    Map<String, Object> map = contactInfoIterator.next();
+                    isSingleModify = map.containsKey("contactinfoID");    
+                }
+            if(isSingleModify){     
+                contactInfoIterator = contactInfo.listIterator();                //Iterator重新生成 
+
+                (partyIdFrom = '1000000371' AND partyRelationshipTypeId IN 
+                    ('AGENCY_REL', 'BOSS_REL', 'SUBORDINATE_REL', 
+                    'COLLEAGUE_REL', 'LOVERS_REL', 'SYMBIOSIS_REL', 'GENERALFRIEND_REL', 'KINDLYFRIEND_REL', 'UNFAMILIAR_REL', 'SICK_REL', 'OTHER_REL', 
+                    'RECO_REL', 'EMP_RECO_REL', 'CUST_RECO_REL', 'PARTNER', 'CHILDREN', 'SELF_PARENTS', 'PARTNER_PARENTS', 
+                    'AGENCY_PARENTS', 'Z_GRANDS', 'W_GRANDS', 'AGENCY_CHILDREN')
+                    AND relationthruDate IS NULL 
+                    AND thruDatePartyContactMech IS NULL 
+                    AND thruDatePartyContactCertificate IS NULL)
+
+                
+
+            
